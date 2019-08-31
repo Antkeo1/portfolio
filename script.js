45bb9af21a4079a03a8758db2a11aba1da4419a7
Antkeo1 <sleav143@gmail.com>
2019-08-31 11:40:38 -0400

    created a carousel using bootstrap

---
+++ script.js
@@ -0,0 +1,28 @@
+// to change toggle button
+$(document).ready(function() {
+    $('.navbar-toggler').click(function() {
+        $('.navbar-toggler').toggleClass('change')
+    })
+    // adding a background and fixed
+    $(window).scroll(function() {
+        let position = $(this).scrollTop();
+        
+        if(position >= 149) {
+            $('.navbar').addClass('navbar-background');
+            $('.navbar').addClass('fixed-top');
+        } else {
+            $('.navbar').removeClass('navbar-background');
+            $('.navbar').removeClass('fixed-top');
+        }
+    })
+    // smooth scroll
+    $('.nav-item a').click(function(link) {
+        link.preventDefault();
+
+        let target = $(this).attr('href');
+        $('html, body').stop().animate({
+            scrollTop: $(target).offset().top 
+        }, 3000);
+    })
+});
+
