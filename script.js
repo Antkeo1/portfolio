cafe8d8720c60a536a525ff785c3bc21cb7e2caf
Antkeo1 <sleav143@gmail.com>
2019-09-01 13:10:48 -0400

    almost complete

--- script.js
+++ script.js
@@ -26,3 +26,17 @@ $(document).ready(function() {
     })
 });
 
+// Isotope
+let $grid = $(".grid").isotope({
+    // options
+  });
+  // filter items on button click
+  $(".filter-button-group").on("click", "button", function() {
+    let filterValue = $(this).attr("data-filter");
+    $grid.isotope({
+      filter: filterValue
+    });
+  });
+});
+
+
